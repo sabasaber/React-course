cd66c1ac04a359c93e2f8e65d0f77cc92e6b0758
sabasaber <75942253+sabasaber@users.noreply.github.com>
2021-09-24 18:47:24 +0330

    Functional component converted to a Class component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
+import React from 'react';
 import Product from './components/Product/Product';
 
-const App = () => {
-    return (
-        <div>
-          <h1 style={{ color: 'orange' }}>Shopping App</h1>
-          <Product num="1" name="React Course" price="99$"/>
-          <Product num="2" name="Node.js Course" price="120$">
-              <p>15% Discount</p>
-              </Product>
-        </div>
-        
-    )
-};
+class App extends React.Component{
+    render(){
+        return (
+            <div>
+              <h1 style={{ color: 'orange' }}>Shopping App</h1>
+              <Product num="1" name="React Course" price="99$"/>
+              <Product num="2" name="Node.js Course" price="120$">
+                  <p>15% Discount</p>
+                  </Product>
+            </div>
+            
+        );
+    }
+}
+
 export default App;
