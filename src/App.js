f5726be57d4fdce81d19c26cea966852accbe142
sabasaber <75942253+sabasaber@users.noreply.github.com>
2021-09-25 14:31:52 +0330

    called HookCounter component

--- src/App.js
+++ src/App.js
@@ -1,64 +1,13 @@
-import React,{useState} from 'react';
-import Product from './components/Product/Product';
-
-// class App extends React.Component{
-
-//     state = {
-//         Products:[
-//             {num:"1", title:"React" , price:"100$"},
-//             {num:"2", title:"Java Script" , price:"90$"},
-//         ],
-//     }
-
-//     clickHandler=()=>{
-//         this.setState({
-//             Products:[
-//                 {num:"1", title:"React" , price:"100$"},
-//                 {num:"2", title:"Java Script" , price:"10$"},
-//             ],
-//         });
-//     }
-
-//     render(){
-//         return (
-//             <div>
-//               <h1 style={{ color: 'orange' }}>Shopping App</h1>
-//               {this.state.Products.map((product)=>{
-//          return <Product num={product.num} name={product.title} price={product.price} />;
-//                 })}
-//                 <button onClick={this.clickHandler}>Click here</button>
-//             </div>
-            
-//         );
-//     }
-// }
-
-// export default App;
+import React from 'react';
+import HookCounter from './components/HookCounter/HookCounter';
 
 const App = () => {
-    const [products,setProducts]=useState(
-        [
-          {num:"1", title:"React" , price:"100$"},
-          {num:"2", title:"Java Script" , price:"90$"},
-        ],
-    );
-
-       const clickHandler=()=>{
-                setProducts(
-                    [
-                        {num:"1", title:"React" , price:"100$"},
-                        {num:"2", title:"Java Script" , price:"10$"},
-                    ],
-                );
-            }
-
+    
     return (<div>
                 <h1 style={{ color: 'orange' }}>Shopping App</h1>
-               {products.map((product)=>{
-                return <Product num={product.num} name={product.title} price={product.price} />;
-                })}
-                <button onClick={clickHandler}>Click here</button>
-            </div> );
+                 <HookCounter/>
+            </div> 
+            );
 }
  
 export default App;
