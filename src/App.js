620fa85b0cf57cd1ec89f03418070e5d2a711841
sabasaber <75942253+sabasaber@users.noreply.github.com>
2021-09-28 12:27:54 +0330

    app.css file imported and used

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
+import Product from './components/Product/Product';
 import React from 'react';
-import HookArray from './components/HookArray/HookArray';
-const App = () => {
-    
-    return (<div>
-                <h1 style={{ color: 'orange' }}>HookArray</h1>
-                 <HookArray/>
-            </div> 
-            );
+
+import './app.css'
+class App extends React.Component{
+
+    state = {
+        Products:[
+            {num:"1", title:"React" , price:"100$"},
+            {num:"2", title:"Java Script" , price:"90$"},
+        ],
+    }
+
+    render(){
+        return (
+            <div className="container">
+              <h1 style={{ color: 'orange' }}>Shopping App</h1>
+              {this.state.Products.map((product)=>{
+                  return <Product num={product.num} name={product.title} price={product.price} />;
+                })}
+                <button className="product">Click here</button>
+            </div>
+
+        );
+    }
 }
- 
 export default App;
