4b1e9a4b9a60aa1255fe3209ae2129f0b1abc1a0
sabasaber <75942253+sabasaber@users.noreply.github.com>
2021-09-29 13:35:20 +0330

    ProductList component imported

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import Product from './components/Product/Product';
 import React from 'react';
+import ProductList from './components/Product/ProductList';
 
 import './app.css'
 class App extends React.Component{
 
     state = {
-        Products:[
-            {num:"1", title:"React" , price:"100$", key:"1"},
-            {num:"2", title:"Java Script" , price:"90$", key:"2"},
-        ],
         count:0,
     }
 
@@ -22,10 +18,8 @@ class App extends React.Component{
         return (
             <div className="container">
               <h1 style={{ color: 'orange' }}>Shopping App</h1>
-              {this.state.Products.map((product)=>{
-                  return <Product num={product.num} name={product.title} price={product.price} key={product.key} click={()=>this.countHandler('product was clicked')}/>;
-                })}
-                <button onClick={()=>this.countHandler(2)} className="product">Click here</button>
+              <ProductList/>
+              <button onClick={()=>this.countHandler(2)} className="product">Click here</button>
             </div>
 
         );
