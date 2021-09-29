164441e81ba0257676a51ed7853786f1c336951d
sabasaber <75942253+sabasaber@users.noreply.github.com>
2021-09-29 13:32:34 +0330

    ProductList component added

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,13 +2,13 @@ import styles from './product.module.css';
 
 const Product = (props) => {
     return ( 
-            <>
+            
               <div className={styles.product} onClick={props.click}>
                 <p>Product {props.num}: {props.name}</p>
                 <p>Price: {props.price}</p>
                 <h6 style={{color:"red"}}>{props.children}</h6>
               </div>
-            </>
+            
      );
 };
  
