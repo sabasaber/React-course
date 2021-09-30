1e4f8a78d7282e08e2609194edbc9016f487441f
sabasaber <75942253+sabasaber@users.noreply.github.com>
2021-09-30 19:44:17 +0330

    button deleted/h1 color changed/state deleted

--- src/App.js
+++ src/App.js
@@ -3,23 +3,12 @@ import ProductList from './components/Product/ProductList';
 
 import './app.css'
 class App extends React.Component{
-
-    state = {
-        count:0,
-    }
-
-   
-    countHandler=(id)=>{
-        
-       console.log(id); 
-    }
-
+    
     render(){
         return (
             <div className="container">
-              <h1 style={{ color: 'orange' }}>Shopping App</h1>
+              <h1 style={{ color: '#424874' }}>Shopping App</h1>
               <ProductList/>
-              <button onClick={()=>this.countHandler(2)} className="product">Click here</button>
             </div>
 
         );
