ddb5f9fa7c2edc6f40d23da0ac04ef30b4313fa9
sabasaber <75942253+sabasaber@users.noreply.github.com>
2021-09-30 19:51:07 +0330

    deleteHandler function created

--- src/components/Product/ProductList.js
+++ src/components/Product/ProductList.js
@@ -7,21 +7,22 @@ class ProductList extends React.Component {
 
     state={
         Products:[
-                  {num:"1", title:"React" , price:"100$", key:"1"},
-                  {num:"2", title:"Java Script" , price:"90$", key:"2"},
-                  {num:"1", title:"NodeJs" , price:"250$", key:"3"},
+                  {num:"1", title:"React.js course" , price:"100$", key:"1"},
+                  {num:"2", title:"Java Script course" , price:"90$", key:"2"},
+                  {num:"3", title:"Node.Js course" , price:"250$", key:"3"},
                   ],
     }
 
-    clickHandler=(id)=>{
+    deleteHandler=(id)=>{
 
-        console.log(id);
-    }
+        const filteredstate = this.state.Products.filter((p)=>p.key!==id);
+        this.setState({Products:filteredstate});
+    };
 
     render() { 
         return <div>
             {this.state.Products.map((product)=>{
-                   return <Product num={product.num} name={product.title} price={product.price} key={product.key} click={()=>this.clickHandler('product was clicked')}/>;
+                   return <Product num={product.num} name={product.title} price={product.price} key={product.key} delete={()=>this.deleteHandler(product.key)} />;
             })}
         </div>
     }
