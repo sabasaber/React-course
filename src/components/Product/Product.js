e43d77e087611e75b5dff21d49c982b5971ac45a
sabasaber <75942253+sabasaber@users.noreply.github.com>
2021-10-04 20:16:01 +0330

    react icons imported and used

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,4 +1,6 @@
 import styles from './product.module.css';
+import { BiTrash } from "react-icons/bi";
+import { IconContext } from "react-icons";
 
 const Product = (props) => {
     return ( 
@@ -6,9 +8,17 @@ const Product = (props) => {
               <div className={styles.product} onClick={props.click}>
                 <p>Product {props.product.num}: {props.product.title}</p>
                 <p>Price: {props.product.price}</p>
-                <span className={styles.value}>{props.product.quantity}</span>
-                <button className={`${styles.button} ${styles.inc}`} onClick={props.increment}>+</button>
-                <button className={styles.button} onClick={props.decrement}>-</button>
+                <span className={styles.value}>
+                  {props.product.quantity}
+                  </span>
+                <button className={`${styles.button} ${styles.inc}`} onClick={props.increment}>
+                  +
+                  </button>
+                <button className={styles.button} onClick={props.decrement} >
+                <IconContext.Provider value={{ size: "1.5em" }}>
+                  {props.product.quantity>1 ? "-" : <BiTrash/>}
+                  </IconContext.Provider>
+                  </button>
                 <button className={styles.button} onClick={props.delete}>Delete</button>
 
               </div>
